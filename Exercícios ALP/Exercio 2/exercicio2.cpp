2d5cd3023ce73a5c07b5142d71afbccbb38e9778
Zenix <33501925+0Zenix@users.noreply.github.com>
2023-09-07 08:42:35 -0300

    Adicionado cpp do exercício 2

--- Exercícios ALP/Exercio 2/exercicio2.cpp
+++ Exercícios ALP/Exercio 2/exercicio2.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
 int main() { 
-    
+    int num1, num2, num3;
+
+    std::cout << "Digite o primeiro numero: ";
+    std::cin >> num1;
+    std::cout << "Digite o segundo numero: ";
+    std::cin >> num2;
+    std::cout << "Digite o terceiro numero: ";
+    std::cin >> num3;
+
+    std::cout << "O resultado da multiplicação entre os números é: " << num1*num2*num3 << std::endl;
+
 }
